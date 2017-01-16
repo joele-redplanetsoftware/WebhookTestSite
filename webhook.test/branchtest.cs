df4a6a3a18daa4a6f6f9fc8226a2115ede4d544b
eugeniopaguntalan <eugeniop@redplanetsoftware.com>
2017-01-16 15:38:03 +0800

    test

---
+++ webhook.test/branchtest.cs
@@ -0,0 +1,45 @@
+﻿using LibGit2Sharp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webhook.test
+{
+    [TestFixture]
+    public class branchtest
+    {
+        [Test]
+        public void testbranch()
+        {
+           
+            //string repoPath = @"C:\Red Planet\webhook_test2";
+            const string url = "https://github.com/joele-redplanetsoftware/WebhookTestSite.git";
+            const string remoteName = "origin";
+
+            string repoPath = @"C:\Red Planet\webhook_test2";
+
+            using (var repo = new Repository(repoPath))
+            {
+                Remote remote = repo.Network.Remotes.Add(remoteName, url);
+                IList<Reference> references = repo.Network.ListReferences(remote).ToList();
+
+
+                foreach (var reference in references)
+                {
+                    // None of those references point to an existing
+                    // object in this brand new repository
+                    Assert.Null(reference.ResolveToDirectReference().Target);
+                }
+
+                List<Tuple<string, string>> actualRefs = references.
+                    Select(directRef => new Tuple<string, string>(directRef.CanonicalName, directRef.ResolveToDirectReference()
+                        .TargetIdentifier)).ToList();
+
+             
+            }
+        }
+    }
+}
---
+++ webhook.test/webhootest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using LibGit2Sharp.Core;
+using LibGit2Sharp.Core.Handles;
+using LibGit2Sharp.Handlers;
+using LibGit2Sharp;
+
+namespace webhook.test
+{
+    public class webhootest
+    {
+        public void GetBranches()
+        {
+            string url = "https://github.com/joele-redplanetsoftware/WebhookTestSite.git";
+
+            // Location on the disk where the local repository should be cloned
+            string workingDirectory = @"C:\Red Planet\webhook_test2";
+
+            // Perform the initial clone
+            string repoPath = Repository.Clone(url, workingDirectory);
+
+            using (var repo = new Repository(repoPath))
+            {
+
+            }
+          
+        }
+    }
+}
